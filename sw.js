4dc7cfcb7203de7715ab3a8148703f3b7cc8d2f0
lorenze1 <153464624+lorenze1@users.noreply.github.com>
2026-08-06 17:01:54 -0700

    Deploy Albert DTS v1.2.1 voice and proposal improvements
    * Update app.js for Albert DTS v1.2.1
    
    * Update index.html for Albert DTS v1.2.1
    
    * Update manifest.webmanifest for Albert DTS v1.2.1
    
    * Update sw.js for Albert DTS v1.2.1
    
    * Add Albert DTS proposal preview styles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='albert-v1.1.1';
-const ASSETS=['/','/index.html','/styles.css','/review.css','/app.js','/manifest.webmanifest','/icons/albert-192.svg','/icons/albert-512.svg'];
+const CACHE='albert-v1.2.1';
+const ASSETS=['/','/index.html','/styles.css','/review.css','/enhancements.css','/app.js','/manifest.webmanifest','/icons/albert-192.svg','/icons/albert-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
