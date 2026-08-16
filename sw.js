ede6e91c833adfe5b126bc601fd59df98a4453f6
lorenze1 <153464624+lorenze1@users.noreply.github.com>
2026-08-16 09:17:15 -0700

    Separate general and small proposal behavior in sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE='albert-v1.6.1';
+const CACHE='albert-v1.6.0';
 const ASSETS=['/','/index.html','/styles.css','/review.css','/enhancements.css','/app.js','/manifest.webmanifest','/icons/albert-192.svg','/icons/albert-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
